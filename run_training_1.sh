9cdd82db6898a511ffbde471776bbb7c421664ea
Ethan <e.goan@student.qut.edu.au>
2017-03-13 09:38:13 +1000

    added training files for training submission

---
+++ run_training_1.sh
@@ -0,0 +1,2 @@
+preprocessing=docker.synapse.org/syn8282795/sub_1@sha256:99105af248c9239bbdf19702224d788bdaebbc153969bc02eda0ea84df25ae95
+training=docker.synapse.org/syn8282795/sub_1_train@sha256:
---
+++ train.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#variables that can be used for setting paths and creating directories
+SAVE_DIR_ROOT="/preprocessedData"
+SAVE_IMAGE_DIR="$SAVE_DIR_ROOT/preprocessedTrain/"
+SAVE_DIR_MODEL_FILES="$SAVE_IMAGE_DIR/model_data/"
+LOG_DIR="/modelState"
+INPUT_DIR="/trainingData/"
+METADATA_DIR="/metadata/"
+MODEL_STATE_DIR="/modelState"
+
+echo "TRAIINING PART OF THE MODEL"
+echo "Running Preprocessing script"
+python main_thread.py -t -f -i $SAVE_IMAGE_DIR -s $SAVE_IMAGE_DIR -l $LOG_DIR -m $METADATA_DIR -a $SAVE_DIR_MODEL_FILES
+
+
+#save the model in the modelstate dir
+#echo "Copying the model to the modelState dir"
+cp $SAVE_DIR_MODEL_FILES/model_file/ $MODEL_STATE_DIR/model_file/ -r -f
+
+sudo python main_thread.py -t -i /media/dperrin/preprocessed/preprocessedTrain/ -s /media/dperrin/preprocessed/preprocessedTrain/ -l ./ -m ./  -a /media/dperrin/preprocessed/preprocessedTrain/model_data/ -f
+
+
+echo "DONE"
+echo "Preprocessing Run Successfully. You may now DANCE :)"
